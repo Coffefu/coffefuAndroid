352cab7119ffd689c8633e5282de3d2e7079f4dd
hedgehog319 <fign1910@mail.ru>
2021-11-12 16:07:54 +1000

    * Fix Fragment refresh

--- app/src/main/java/com/example/coffefu/MainActivity.kt
+++ app/src/main/java/com/example/coffefu/MainActivity.kt
@@ -4,38 +4,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val menu = Menu()
+    private val basket = Basket()
+    private val feedback = Feedback()
+
+    private var active: Fragment = menu
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // TODO дописать логику фрагементов
-        // https://newbedev.com/stop-fragment-refresh-in-bottom-nav-using-navhost
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_bar)
 
-        val firstFragment = Menu()
-        val secondFragment = Basket()
-        val thirdFragment = Feedback()
-
-        setCurrentFragment(firstFragment)
+        supportFragmentManager.beginTransaction().add(R.id.flFragment, menu).commit()
+        supportFragmentManager.beginTransaction().add(R.id.flFragment, basket).hide(basket).commit()
+        supportFragmentManager.beginTransaction().add(R.id.flFragment, feedback).hide(feedback)
+            .commit()
 
         bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.menu->setCurrentFragment(firstFragment)
-                R.id.basket->setCurrentFragment(secondFragment)
-                R.id.feedback->setCurrentFragment(thirdFragment)
+            when (it.itemId) {
+                R.id.menu -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(menu)
+                        .commit()
+                    active = menu
+                }
 
+                R.id.basket -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(basket)
+                        .commit()
+                    active = basket
+                }
+
+                R.id.feedback -> {
+                    supportFragmentManager.beginTransaction().hide(active).show(feedback)
+                        .commit()
+                    active = feedback
+                }
             }
             true
         }
 
     }
-
-    private fun setCurrentFragment(fragment: Fragment)=
-        supportFragmentManager.beginTransaction().apply {
-            replace( R.id.flFragment, fragment)
-            commit()
-        }
 }
